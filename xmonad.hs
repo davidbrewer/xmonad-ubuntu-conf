eba72a97d8fefb158215a3df533413bb5735395b
David Brewer <dbrewer@secondstory.com>
2012-07-10 15:34:24 -0700

    Oops... looks like we still need "noBorders" on the full layout to avoid a border.

--- xmonad.hs
+++ xmonad.hs
@@ -2,11 +2,11 @@
   This is my xmonad configuration file.
   There are many like it, but this one is mine.
 
-  If you want to customize this file, the easiest workflow goes 
+  If you want to customize this file, the easiest workflow goes
   something like this:
     1. Make a small change.
     2. Hit "super-q", which recompiles and restarts xmonad
-    3. If there is an error, undo your change and hit "super-q" again to 
+    3. If there is an error, undo your change and hit "super-q" again to
        get to a stable place again.
     4. Repeat
 
@@ -33,7 +33,7 @@ import qualified Data.Map as M
 import Data.Ratio ((%))
 
 {-
-  Xmonad configuration variables. These settings control some of the 
+  Xmonad configuration variables. These settings control some of the
   simpler parts of xmonad's behavior and are straightforward to tweak.
 -}
 
@@ -46,8 +46,8 @@ myIMRosterTitle      = "Contact List" -- title of roster on IM workspace
 
 
 {-
-  Xmobar configuration variables. These settings control the appearance 
-  of text which xmonad is sending to xmobar via the DynamicLog hook. 
+  Xmobar configuration variables. These settings control the appearance
+  of text which xmonad is sending to xmobar via the DynamicLog hook.
 -}
 
 myTitleColor     = "#eeeeee"  -- color of window title
@@ -63,7 +63,7 @@ myVisibleWSRight = ")"
 {-
   Workspace configuration. Here you can change the names of your
   workspaces. Note that they are organized in a grid corresponding
-  to the layout of the number pad. 
+  to the layout of the number pad.
 
   I would recommend sticking with relatively brief workspace names
   because they are displayed in the xmobar status bar, where space
@@ -71,12 +71,12 @@ myVisibleWSRight = ")"
   in the configuration file, so when you change a label you will have
   to find places which refer to it and make a change there as well.
 
-  This central organizational concept of this configuration is that 
-  the workspaces correspond to keys on the number pad, and that they 
-  are organized in a grid which also matches the layout of the number pad. 
-  So, I don't recommend changing the number of workspaces unless you are 
-  prepared to delve into the workspace navigation keybindings section 
-  as well. 
+  This central organizational concept of this configuration is that
+  the workspaces correspond to keys on the number pad, and that they
+  are organized in a grid which also matches the layout of the number pad.
+  So, I don't recommend changing the number of workspaces unless you are
+  prepared to delve into the workspace navigation keybindings section
+  as well.
 -}
 
 myWorkspaces =
@@ -91,21 +91,21 @@ startupWorkspace = "5:Dev"  -- which workspace do you want to be on after launch
 
 {-
   Layout configuration. In this section we identify which xmonad
-  layouts we want to use. I have defined a list of default 
-  layouts which are applied on every workspace, as well as 
+  layouts we want to use. I have defined a list of default
+  layouts which are applied on every workspace, as well as
   special layouts which get applied to specific workspaces.
 
   Note that all layouts are wrapped within "avoidStruts". What this does
   is make the layouts avoid the status bar area at the top of the screen.
   Without this, they would overlap the bar. You can toggle this behavior
   by hitting "super-b" (bound to ToggleStruts in the keyboard bindings
-  in the next section). 
+  in the next section).
 -}
 
 -- Define group of default layouts used on most screens, in the
 -- order they will appear.
 -- "smartBorders" modifier makes it so the borders on windows only
--- appear if there is more than one visible window. 
+-- appear if there is more than one visible window.
 -- "avoidStruts" modifier makes it so that the layout provides
 -- space for the status bar at the top of the screen.
 defaultLayouts = smartBorders(avoidStruts(
@@ -118,30 +118,30 @@ defaultLayouts = smartBorders(avoidStruts(
   -- Mirrored variation of ResizableTall. In this layout, the large
   -- master window is at the top, and remaining windows tile at the
   -- bottom of the screen. Can be resized as described above.
-  ||| Mirror (ResizableTall 1 (3/100) (1/2) []) 
+  ||| Mirror (ResizableTall 1 (3/100) (1/2) [])
 
-  -- Full layout makes every window full screen. When you toggle the 
+  -- Full layout makes every window full screen. When you toggle the
   -- active window, it will bring the active window to the front.
-  ||| Full
+  ||| noBorders Full
 
   -- Grid layout tries to equally distribute windows in the available
   -- space, increasing the number of columns and rows as necessary.
   -- Master window is at top left.
-  ||| Grid 
+  ||| Grid
 
   -- ThreeColMid layout puts the large master window in the center
   -- of the screen. As configured below, by default it takes of 3/4 of
   -- the available space. Remaining windows tile to both the left and
-  -- right of the master window. You can resize using "super-h" and 
+  -- right of the master window. You can resize using "super-h" and
   -- "super-l".
   ||| ThreeColMid 1 (3/100) (3/4)
 
-  -- Circle layout places the master window in the center of the screen. 
+  -- Circle layout places the master window in the center of the screen.
   -- Remaining windows appear in a circle around it
   ||| Circle))
 
 
--- Here we define some layouts which will be assigned to specific 
+-- Here we define some layouts which will be assigned to specific
 -- workspaces based on the functionality of that workspace.
 
 -- The chat layout uses the "IM" layout. We have a roster which takes
@@ -154,17 +154,17 @@ chatLayout = avoidStruts(withIM (1%7) (Title myIMRosterTitle) Grid)
 
 -- The GIMP layout uses the ThreeColMid layout. The traditional GIMP
 -- floating panels approach is a bit of a challenge to handle with xmonad;
--- I find the best solution is to make the image you are working on the 
+-- I find the best solution is to make the image you are working on the
 -- master area, and then use this ThreeColMid layout to make the panels
 -- tile to the left and right of the image. If you use GIMP 2.8, you
 -- can use single-window mode and avoid this issue.
 gimpLayout = smartBorders(avoidStruts(ThreeColMid 1 (3/100) (3/4)))
 
--- Here we combine our default layouts with our specific, workspace-locked 
+-- Here we combine our default layouts with our specific, workspace-locked
 -- layouts.
-myLayouts = 
-  onWorkspace "7:Chat" chatLayout 
-  $ onWorkspace "9:Pix" gimpLayout 
+myLayouts =
+  onWorkspace "7:Chat" chatLayout
+  $ onWorkspace "9:Pix" gimpLayout
   $ defaultLayouts
 
 
@@ -176,7 +176,7 @@ myLayouts =
 
   It can be difficult to find a good list of keycodes for use
   in xmonad. I have found this page useful -- just look
-  for entries beginning with "xK":  
+  for entries beginning with "xK":
 
   http://xmonad.org/xmonad-docs/xmonad/doc-index-X.html
 
@@ -192,7 +192,7 @@ myLayouts =
   the output.
 -}
 
-myKeyBindings = 
+myKeyBindings =
   [
     ((myModMask, xK_b), sendMessage ToggleStruts)
     , ((myModMask, xK_a), sendMessage MirrorShrink)
@@ -208,23 +208,23 @@ myKeyBindings =
   Management hooks. You can use management hooks to enforce certain
   behaviors when specific programs or windows are launched. This is
   useful if you want certain windows to not be managed by xmonad,
-  or sent to a specific workspace, or otherwise handled in a special 
+  or sent to a specific workspace, or otherwise handled in a special
   way.
 
-  Each entry within the list of hooks defines a way to identify a 
+  Each entry within the list of hooks defines a way to identify a
   window (before the arrow), and then how that window should be treated
-  (after the arrow). 
+  (after the arrow).
 
-  To figure out to identify your window, you will need to use a 
+  To figure out to identify your window, you will need to use a
   command-line tool called "xprop". When you run xprop, your cursor
   will temporarily change to crosshairs; click on the window you
   want to identify. In the output that is printed in your terminal,
   look for a couple of things:
     - WM_CLASS(STRING): values in this list of strings can be compared
-      to "className" to match windows. 
+      to "className" to match windows.
     - WM_NAME(STRING): this value can be compared to "resource" to match
       windows.
-  
+
   The className values tend to be generic, and might match any window or
   dialog owned by a particular program. The resource values tend to be
   more specific, and will be different for every dialog. Sometimes you
@@ -243,7 +243,7 @@ myKeyBindings =
     - doF (W.shift "Workspace"): this tells xmonad that when this program
       is launched it should be sent to a specific workspace. Useful
       for keeping specific tasks on specific workspaces. In the example
-      below I have specific workspaces for chat, development, and 
+      below I have specific workspaces for chat, development, and
       editing images.
 -}
 
@@ -268,7 +268,7 @@ myManagementHooks = [
   the least. Be very careful if messing with this section.
 -}
 
--- We define two lists of keycodes for use in the rest of the 
+-- We define two lists of keycodes for use in the rest of the
 -- keyboard configuration. The first is the list of numpad keys,
 -- in the order they occur on the keyboard (left to right and
 -- top to bottom). The second is the list of number keys, in an
@@ -276,7 +276,7 @@ myManagementHooks = [
 -- make workspace navigation commands work the same whether you
 -- use the numpad or the top-row number keys. And, we also
 -- use them to figure out where to go when the user
--- uses the arrow keys. 
+-- uses the arrow keys.
 numPadKeys =
   [
     xK_KP_Home, xK_KP_Up, xK_KP_Page_Up
@@ -311,16 +311,16 @@ myKeys = myKeyBindings ++
   ] ++
   M.toList (planeKeys myModMask (Lines 4) Finite) ++
   [
-    ((m .|. myModMask, key), screenWorkspace sc 
+    ((m .|. myModMask, key), screenWorkspace sc
       >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2] 
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
   ]
 
 
 {-
   Here we actually stitch together all the configuration settings
-  and run xmonad. We also spawn an instance of xmobar and pipe 
+  and run xmonad. We also spawn an instance of xmobar and pipe
   content into it via the logHook..
 -}
 
@@ -338,15 +338,15 @@ main = do
   , startupHook = do
       windows $ W.greedyView startupWorkspace
       spawn "~/.xmonad/startup-hook"
-  , manageHook = manageHook defaultConfig 
-      <+> composeAll myManagementHooks 
+  , manageHook = manageHook defaultConfig
+      <+> composeAll myManagementHooks
       <+> manageDocks
-  , logHook = dynamicLogWithPP $ xmobarPP { 
+  , logHook = dynamicLogWithPP $ xmobarPP {
       ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor myTitleColor "" . shorten myTitleLength
-      , ppCurrent = xmobarColor myCurrentWSColor "" 
+      , ppCurrent = xmobarColor myCurrentWSColor ""
         . wrap myCurrentWSLeft myCurrentWSRight
-      , ppVisible = xmobarColor myVisibleWSColor "" 
+      , ppVisible = xmobarColor myVisibleWSColor ""
         . wrap myVisibleWSLeft myVisibleWSRight
     }
   }
