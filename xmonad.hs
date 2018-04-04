a151d481d628c5a8f0aeef0a9972c54696c3b0ac
David Brewer <dbrewer@secondstory.com>
2018-04-04 09:28:41 -0700

    Switched to using gnome-terminal as default terminal.

--- xmonad.hs
+++ xmonad.hs
@@ -44,7 +44,7 @@ myModMask            = mod4Mask       -- changes the mod key to "super"
 myFocusedBorderColor = "#ff0000"      -- color of focused border
 myNormalBorderColor  = "#cccccc"      -- color of inactive border
 myBorderWidth        = 1              -- width of border around windows
-myTerminal           = "terminator"   -- which terminal software to use
+myTerminal           = "gnome-terminal"   -- which terminal software to use
 myIMRosterTitle      = "Buddy List"   -- title of roster on IM workspace
                                       -- use "Buddy List" for Pidgin, but
                                       -- "Contact List" for Empathy
